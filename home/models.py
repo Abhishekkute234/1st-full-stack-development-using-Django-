d2445dcee7cd6865d14bdc786477c4be518e7865
Amazon Clone <kuteabhishek2004@gmail.com>
2024-03-16 08:41:12 +0530

    add python files

---
+++ home/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# makemigrations - create changes and store in a file 
+# migrate - apply the pending changes created by makemigrations
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
+
+
+# to see who register (to chnage the object name in migration)
+    def __str__(self):
+        return self.name
+    
